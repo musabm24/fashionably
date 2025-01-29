0f489641cd5a9ccaac2d302ae9e29932a7156ae4
musabm24 <m.mohamed21@wlv.ac.uk>
2025-01-29 19:34:23 +0000

    Fixed gitignore and cleaned repo

---
+++ myProject/myApp/static/js/script.js
@@ -0,0 +1,6 @@
+// script.js
+
+function changeContent() {
+    // Change the text of the paragraph when the button is clicked
+    document.getElementById("myParagraph").innerText = "You clicked the button! The content has changed.";
+}
